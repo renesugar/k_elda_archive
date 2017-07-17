69d8b87c156e91e4d9d0a39aaf1ffe8f0ee910ce
Kevin Lin <kevin@quilt.io>
2017-07-17 09:56:25 -0700

    ci: Retry the stop test on failure
    Every once in awhile, the stop test has been failing because the leader
    never gets the stop spec from the daemon. This might be because the
    test isn't sleeping long enough for the daemon to send the spec.
    
    Rather than try to tweak the timeout, this patch modifies the test so
    that we check up to 5 times whether the containers were stopped. Note
    that we still fail if the test doesn't succeed on the first try, but the
    additional data from extra runs will be useful for debugging.

--- quilt-tester/tests/40-stop/stop_test.go
+++ quilt-tester/tests/40-stop/stop_test.go
@@ -19,34 +19,57 @@ func TestStopContainer(t *testing.T) {
 		t.Fatalf("couldn't run stop command: %s", err.Error())
 	}
 
-	log.Info("Sleeping thirty seconds for `quilt stop -containers` to take effect")
-	time.Sleep(30 * time.Second)
-
 	c, err := client.New(api.DefaultSocket)
 	if err != nil {
 		t.Fatalf("couldn't get quiltctl client: %s", err.Error())
 	}
 	defer c.Close()
 
-	machines, err := c.QueryMachines()
-	if err != nil {
-		t.Fatalf("couldn't query machines: %s", err.Error())
+	var passed bool
+	tries := 1
+	for ; tries <= 5; tries++ {
+		log.Info("Sleeping thirty seconds for `quilt stop -containers` " +
+			"to take effect")
+		time.Sleep(30 * time.Second)
+
+		machines, err := c.QueryMachines()
+		if err != nil {
+			t.Fatalf("couldn't query machines: %s", err.Error())
+		}
+
+		passed = true
+		for _, m := range machines {
+			containersRaw, err := exec.Command("quilt", "ssh", m.StitchID,
+				"docker", "ps", "--format", "{{.Names}}").Output()
+			if err != nil {
+				passed = false
+				t.Errorf("couldn't run `docker ps`: %s", err.Error())
+				continue
+			}
+
+			containersStr := strings.TrimSpace(string(containersRaw))
+			fmt.Printf("Containers on machine %s:\n", m.StitchID)
+			fmt.Println(string(containersRaw))
+
+			names := strings.Split(containersStr, "\n")
+			if len(filterQuiltContainers(names)) > 0 {
+				passed = false
+				t.Errorf("machine %s has unexpected containers",
+					m.StitchID)
+			}
+		}
+
+		if passed {
+			break
+		}
 	}
 
-	for _, m := range machines {
-		containersRaw, err := exec.Command("quilt", "ssh", m.StitchID,
-			"docker", "ps", "--format", "{{.Names}}").Output()
-		if err != nil {
-			t.Errorf("couldn't run `docker ps`: %s", err.Error())
-			continue
-		}
-
-		fmt.Printf("Containers on machine %s:\n", m.StitchID)
-		fmt.Println(string(containersRaw))
-		names := strings.Split(strings.TrimSpace(string(containersRaw)), "\n")
-		if len(filterQuiltContainers(names)) > 0 {
-			t.Errorf("machine %s has unexpected containers", m.StitchID)
-		}
+	switch {
+	case tries > 1 && passed:
+		t.Logf("Although the containers weren't stopped on the first check, "+
+			"they eventually stopped on try %d.", tries)
+	case !passed:
+		t.Error("Some containers were never stopped")
 	}
 }
 
