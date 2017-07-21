16bcce16010f4b74cfc0a086c8382045b6d306fc
Kay Ousterhout <kay@quilt.io>
2017-07-20 21:28:03 -0700

    tester: Update use of nginx
    The nginx spec changed to export a function createService
    instead of New to instantiate a new nginx service.

--- quilt-tester/tests/inbound-public/inbound-public.js
+++ quilt-tester/tests/inbound-public/inbound-public.js
@@ -7,7 +7,6 @@ let deployment = createDeployment({});
 deployment.deploy(infrastructure);
 
 for (let i = 0; i < infrastructure.nWorker; i++) {
-     /* eslint new-cap: "warn"*/
-    deployment.deploy(nginx.New(80));
-    deployment.deploy(nginx.New(8000));
+    deployment.deploy(nginx.createService(80));
+    deployment.deploy(nginx.createService(8000));
 }
