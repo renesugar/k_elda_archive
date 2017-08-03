54ca1e7ed6d3199020d8758ada1bd82739e51693
Kevin Lin <kevin@quilt.io>
2017-08-03 14:24:09 -0700

    ci: Remove reference to Spark.exclusive
    It's going to be removed.

--- quilt-tester/tests/20-spark/spark.js
+++ quilt-tester/tests/20-spark/spark.js
@@ -3,12 +3,10 @@ let spark = require('@quilt/spark');
 let infrastructure = require('../../config/infrastructure.js');
 
 // Application
-// sprk.exclusive enforces that no two Spark containers should be on the same
-// node. sprk.exposeUIToPublic says that the the public internet should be able
+// sprk.exposeUIToPublic says that the the public internet should be able
 // to connect to the Spark web interface. sprk.job causes Spark to run that
 // job when it boots.
 const sprk = new spark.Spark(1, infrastructure.nWorker-1)
-  .exclusive()
   .exposeUIToPublic()
   .job('run-example SparkPi');
 
