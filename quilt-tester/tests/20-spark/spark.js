52a798a82531cb078f0b966ec87f1d41c24293d9
Kevin Lin <kevin@quilt.io>
2017-08-03 11:14:00 -0700

    ci: Use new Spark API in Spark test
    The Spark library renamed `Spark.public` to `Spark.exposeUIToPublic`.

--- quilt-tester/tests/20-spark/spark.js
+++ quilt-tester/tests/20-spark/spark.js
@@ -3,14 +3,14 @@ let spark = require('@quilt/spark');
 let infrastructure = require('../../config/infrastructure.js');
 
 // Application
-// sprk.exclusive enforces that no two Spark containers should be on the
-// same node. sprk.public says that the containers should be allowed to talk
-// on the public internet. sprk.job causes Spark to run that job when it
-// boots.
-let sprk = new spark.Spark(1, infrastructure.nWorker-1)
-    .exclusive()
-    .public()
-    .job('run-example SparkPi');
+// sprk.exclusive enforces that no two Spark containers should be on the same
+// node. sprk.exposeUIToPublic says that the the public internet should be able
+// to connect to the Spark web interface. sprk.job causes Spark to run that
+// job when it boots.
+const sprk = new spark.Spark(1, nWorker)
+  .exclusive()
+  .exposeUIToPublic()
+  .job('run-example SparkPi');
 
 let deployment = createDeployment({});
 deployment.deploy(infrastructure);
