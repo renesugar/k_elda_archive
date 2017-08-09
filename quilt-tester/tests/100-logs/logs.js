83047ca20449542a4e6556e8caea5a881898091f
Kevin Lin <kevin@quilt.io>
2017-08-08 20:40:01 -0700

    ci: Remove containers from log test
    The logs test is used to check whether any of the other integration
    tests caused error messages to be logged -- deploying a container in
    the associated blueprint is unlikely to cause any new errors, and
    distracts from the purpose of the test.

--- quilt-tester/tests/100-logs/logs.js
+++ quilt-tester/tests/100-logs/logs.js
@@ -1,21 +1,5 @@
-const {
-    Container,
-    PortRange,
-    Service,
-    createDeployment} = require('@quilt/quilt');
+const {createDeployment} = require('@quilt/quilt');
 let infrastructure = require('../../config/infrastructure.js');
 
-let deployment = createDeployment({});
+let deployment = createDeployment();
 deployment.deploy(infrastructure);
-
-let nWorker = 1;
-let red = new Service('red', new Container('google/pause').replicate(nWorker));
-let blue = new Service(
-    'blue', new Container('google/pause').replicate(3 * nWorker));
-
-let ports = new PortRange(1024, 65535);
-blue.allowFrom(red, ports);
-red.allowFrom(blue, ports);
-
-deployment.deploy(red);
-deployment.deploy(blue);
