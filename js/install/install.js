ejj/quilt-dev

npm: Organize all of the npm packages into one subdirectory
This patch moves bindings.js, the initializer, and `@quilt/install`
(which was formerly in a different repo), all under one subdiretory.
It also changes `@quilt/install` to use locally built binaries,
instead of pulling them from github.

js/install/install.js
@@ -1,0 +1,14 @@
+const os = require('os');
+const fs = require('fs');
+
+const platform = os.platform();
+switch (platform) {
+  case 'darwin':
+    fs.symlinkSync('./quilt_darwin', './quilt');
+    break;
+  case 'linux':
+    fs.symlinkSync('./quilt_linux', './quilt');
+    break;
+  default:
+    throw new Error(`Unrecognized operating system ${platform}. Bailing.`);
+}

renesugar/k_elda_archive

Co-Authored-By: Kevin Lin <kevin@quilt.io>

js/install/install.js
@@ -1,0 +1,14 @@
+const os = require('os');
+const fs = require('fs');
+
+const platform = os.platform();
+switch (platform) {
+  case 'darwin':
+    fs.symlinkSync('./kelda_darwin', './kelda');
+    break;
+  case 'linux':
+    fs.symlinkSync('./kelda_linux', './kelda');
+    break;
+  default:
+    throw new Error(`Unrecognized operating system ${platform}. Bailing.`);
+}