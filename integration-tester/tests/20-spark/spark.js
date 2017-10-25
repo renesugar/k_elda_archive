867e9f175c0ea32475106c3fe63c947c2d9dd8eb
Kay Ousterhout <kay@kelda.io>
2017-10-25 15:03:50 -0700

    tester: Update Spark comment
    A previous commit removed use of the job() function in Spark;
    this commit updates the comment to reflect this.

--- integration-tester/tests/20-spark/spark.js
+++ integration-tester/tests/20-spark/spark.js
@@ -3,8 +3,7 @@ const infrastructure = require('../../config/infrastructure.js');
 
 // Application
 // sprk.exposeUIToPublic says that the the public internet should be able
-// to connect to the Spark web interface. sprk.job causes Spark to run that
-// job when it boots.
+// to connect to the Spark web interface.
 const sprk = new spark.Spark(infrastructure.nWorker - 1)
   .exposeUIToPublic();
 
