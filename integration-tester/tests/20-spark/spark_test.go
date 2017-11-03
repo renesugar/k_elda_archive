65bf29c6f236b3589d73077272916dc18ea8fd36
Kay Ousterhout <kay@kelda.io>
2017-11-03 14:10:32 -0700

    tester: Improve Spark integration test
    The old Spark integration test computed Pi, which did involve a
    shuffle, so the integration test passed even when Spark wasn't
    configured to correctly allow different Spark workers to
    communicate.  This commit updates the integration test to
    perform a shuffle.

--- integration-tester/tests/20-spark/spark_test.go
+++ integration-tester/tests/20-spark/spark_test.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 	"testing"
 
 	testerUtil "github.com/kelda/kelda/integration-tester/util"
 )
 
-func TestCalculatesPI(t *testing.T) {
+func TestRunShuffleJob(t *testing.T) {
 	clnt, err := testerUtil.GetDefaultDaemonClient()
 	if err != nil {
 		t.Fatalf("couldn't get api client: %s", err.Error())
@@ -36,14 +38,39 @@ func TestCalculatesPI(t *testing.T) {
 		t.Fatal("unable to find BlueprintID of Spark master")
 	}
 
-	cmd := exec.Command("kelda", "ssh", id, "run-example", "SparkPi")
-	output, err := cmd.Output()
-	fmt.Println(string(output))
+	// Run a job that shuffles 100,000 key-value pairs using 100 mappers and 10
+	// reducers.
+	numMappers := 100
+	keyValuePairsPerMapper := 1000
+	valuesPerKey := 4
+	numReducers := 10
+	cmd := exec.Command("kelda", "ssh", id, "run-example",
+		fmt.Sprintf("GroupByTest %d %d %d %d",
+			numMappers, keyValuePairsPerMapper, valuesPerKey, numReducers))
+	stderrBytes := bytes.NewBuffer(nil)
+	cmd.Stderr = stderrBytes
+
+	stdoutBytes, err := cmd.Output()
+	t.Log(stderrBytes.String())
+	fmt.Println(string(stdoutBytes))
 	if err != nil {
 		t.Fatalf("Failed to run spark job: %s", err.Error())
 	}
 
-	if !strings.Contains(string(output), "Pi is roughly") {
-		t.Fatalf("Failed to calculate Pi")
+	outputAsStr := strings.Trim(string(stdoutBytes), " \n\t")
+	outputAsNum, err := strconv.Atoi(outputAsStr)
+	if err != nil {
+		t.Fatalf("Output of Spark job was expected to be a number (was %s)",
+			outputAsStr)
+	}
+
+	// The expected output is the number of unique keys.  Since the keys are randomly
+	// generated, there will be some overlap in the keys, so there won't quite be
+	// numMappers*numKeyValuePairsPerMapper (i.e., 100,000) unique keys.
+	maxOutput := numMappers * keyValuePairsPerMapper
+	minOutput := int(float64(maxOutput) * 0.99)
+	if outputAsNum < minOutput || outputAsNum > maxOutput {
+		t.Fatalf("Spark shuffle job result (%d) was not in the expected range",
+			outputAsNum)
 	}
 }
