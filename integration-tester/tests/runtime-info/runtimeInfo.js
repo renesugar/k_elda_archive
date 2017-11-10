1e27dc591e9a26edea8f5a9f875d2c4998d788e4
Kevin Lin <kevin@kelda.io>
2017-11-10 08:11:56 -0800

    tester: Remove non-determinism from runtime-info test
    Before, the runtime info test picked a random hash to use as its
    index.html at runtime. This resulted in the blueprint producing
    different containers each time it was run -- this confused the
    integration test runner since it checks whether the expected containers
    had been booted by re-compiling the blueprint (which would result in
    a different container than what was actually deployed, causing the test
    to fail).

--- integration-tester/tests/runtime-info/runtimeInfo.js
+++ integration-tester/tests/runtime-info/runtimeInfo.js
@@ -1,6 +1,5 @@
 const kelda = require('kelda');
 const infrastructure = require('../../config/infrastructure.js');
-const crypto = require('crypto');
 
 const infra = infrastructure.createTestInfrastructure();
 
@@ -10,8 +9,13 @@ const webContainer = new kelda.Container('web', 'nginx', {
   env: {
     myPubIP: kelda.hostIP,
   },
+  // Serve a random hash so that the test can ensure it really reached this
+  // container. Although it would be nice for the hash to be randomly generated
+  // at runtime, the blueprint would then produce two different containers when
+  // run twice. This would confuse the integration-tester because it would think
+  // that the container from the first run never booted.
   filepathToContent: {
-    [indexPath]: crypto.randomBytes(20).toString('hex'),
+    [indexPath]: 'bb48133faa41ecb860ffce0f4ee09a82',
   },
 });
 
