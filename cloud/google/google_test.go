c451a2218ca0566ed706d6ba39e9799992b732c2
Ethan J. Jackson <ethan@kelda.io>
2017-12-03 15:52:42 -0700

    google: Refactor google_test
    The google_test.go file uses a test suite functionality that is not
    common in our code base, more verbose than what we typically do, and
    doesn't support all of the features we typically use (notably
    mock.Anything).  This patch replaces it with something a bit more
    typical.

--- cloud/google/google_test.go
+++ cloud/google/google_test.go
@@ -7,30 +7,25 @@ import (
 	"github.com/kelda/kelda/cloud/acl"
 	"github.com/kelda/kelda/cloud/google/client/mocks"
 	"github.com/kelda/kelda/db"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/assert"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-type GoogleTestSuite struct {
-	suite.Suite
-
-	gce *mocks.Client
-	*Provider
-}
-
-func (s *GoogleTestSuite) SetupTest() {
-	s.gce = new(mocks.Client)
-	s.Provider = &Provider{
-		Client:    s.gce,
+func getProvider() (*mocks.Client, Provider) {
+	mc := new(mocks.Client)
+	return mc, Provider{
+		Client:    mc,
 		namespace: "namespace",
 		network:   "network",
 		zone:      "zone-1",
+		intFW:     "intFW",
 	}
 }
 
-func (s *GoogleTestSuite) TestList() {
-	s.gce.On("ListInstances", "zone-1", "network").Return(&compute.InstanceList{
+func TestList(t *testing.T) {
+	mc, gce := getProvider()
+	mc.On("ListInstances", "zone-1", "network").Return(&compute.InstanceList{
 		Items: []*compute.Instance{
 			{
 				MachineType: "machine/split/type-1",
@@ -49,10 +44,10 @@ func (s *GoogleTestSuite) TestList() {
 		},
 	}, nil)
 
-	machines, err := s.List()
-	s.NoError(err)
-	s.Len(machines, 1)
-	s.Equal(machines[0], db.Machine{
+	machines, err := gce.List()
+	assert.NoError(t, err)
+	assert.Len(t, machines, 1)
+	assert.Equal(t, machines[0], db.Machine{
 		Provider:  "Google",
 		Region:    "zone-1",
 		CloudID:   "name-1",
@@ -62,44 +57,44 @@ func (s *GoogleTestSuite) TestList() {
 	})
 }
 
-func (s *GoogleTestSuite) TestListFirewalls() {
-	s.network = "network"
-	s.intFW = "intFW"
-
-	s.gce.On("ListFirewalls", s.network).Return(&compute.FirewallList{
+func TestListFirewalls(t *testing.T) {
+	mc, gce := getProvider()
+	mc.On("ListFirewalls", gce.network).Return(&compute.FirewallList{
 		Items: []*compute.Firewall{
 			{
-				Network:    s.networkURL(),
+				Network:    gce.networkURL(),
 				Name:       "badZone",
 				TargetTags: []string{"zone-2"},
 			},
 			{
-				Network:    s.networkURL(),
+				Network:    gce.networkURL(),
 				Name:       "intFW",
 				TargetTags: []string{"zone-1"},
 			},
 			{
-				Network:    s.networkURL(),
+				Network:    gce.networkURL(),
 				Name:       "shouldReturn",
 				TargetTags: []string{"zone-1"},
 			},
 		},
 	}, nil).Once()
 
-	fws, err := s.listFirewalls()
-	s.NoError(err)
-	s.Len(fws, 1)
-	s.Equal(fws[0].Name, "shouldReturn")
+	fws, err := gce.listFirewalls()
+	assert.NoError(t, err)
+	assert.Len(t, fws, 1)
+	assert.Equal(t, fws[0].Name, "shouldReturn")
 
-	s.gce.On("ListFirewalls", s.network).Return(nil, errors.New("err")).Once()
-	_, err = s.listFirewalls()
-	s.EqualError(err, "list firewalls: err")
+	mc.On("ListFirewalls", gce.network).Return(nil, errors.New("err")).Once()
+	_, err = gce.listFirewalls()
+	assert.EqualError(t, err, "list firewalls: err")
 }
 
-func (s *GoogleTestSuite) TestListBadNetworkInterface() {
+func TestListBadNetworkInterface(t *testing.T) {
+	mc, gce := getProvider()
+
 	// Tests that List returns an error when no network interfaces are
 	// configured.
-	s.gce.On("ListInstances", "zone-1", "network").Return(&compute.InstanceList{
+	mc.On("ListInstances", "zone-1", "network").Return(&compute.InstanceList{
 		Items: []*compute.Instance{
 			{
 				MachineType:       "machine/split/type-1",
@@ -109,13 +104,14 @@ func (s *GoogleTestSuite) TestListBadNetworkInterface() {
 		},
 	}, nil)
 
-	_, err := s.List()
-	s.EqualError(err, "Google instances are expected to have exactly 1 "+
+	_, err := gce.List()
+	assert.EqualError(t, err, "Google instances are expected to have exactly 1 "+
 		"interface; for instance name-1, found 0")
 }
 
-func (s *GoogleTestSuite) TestParseACLs() {
-	parsed, err := s.parseACLs([]compute.Firewall{
+func TestParseACLs(t *testing.T) {
+	_, gce := getProvider()
+	parsed, err := gce.parseACLs([]compute.Firewall{
 		{
 			Name: "firewall",
 			Allowed: []*compute.FirewallAllowed{
@@ -131,8 +127,8 @@ func (s *GoogleTestSuite) TestParseACLs() {
 			SourceRanges: []string{"foo"},
 		},
 	})
-	s.NoError(err)
-	s.Equal([]acl.ACL{
+	assert.NoError(t, err)
+	assert.Equal(t, []acl.ACL{
 		{MinPort: 80, MaxPort: 80, CidrIP: "foo"},
 		{MinPort: 20, MaxPort: 25, CidrIP: "foo"},
 		{MinPort: 80, MaxPort: 80, CidrIP: "bar"},
@@ -140,7 +136,7 @@ func (s *GoogleTestSuite) TestParseACLs() {
 		{MinPort: 1, MaxPort: 65535, CidrIP: "foo"},
 	}, parsed)
 
-	_, err = s.parseACLs([]compute.Firewall{
+	_, err = gce.parseACLs([]compute.Firewall{
 		{
 			Name: "firewall",
 			Allowed: []*compute.FirewallAllowed{
@@ -149,10 +145,10 @@ func (s *GoogleTestSuite) TestParseACLs() {
 			SourceRanges: []string{"foo"},
 		},
 	})
-	s.EqualError(err, `parse ports of firewall: parse ints: strconv.Atoi: `+
+	assert.EqualError(t, err, `parse ports of firewall: parse ints: strconv.Atoi: `+
 		`parsing "NaN": invalid syntax`)
 
-	_, err = s.parseACLs([]compute.Firewall{
+	_, err = gce.parseACLs([]compute.Firewall{
 		{
 			Name: "firewall",
 			Allowed: []*compute.FirewallAllowed{
@@ -161,9 +157,6 @@ func (s *GoogleTestSuite) TestParseACLs() {
 			SourceRanges: []string{"foo"},
 		},
 	})
-	s.EqualError(err, "parse ports of firewall: unrecognized port format: 1-80-81")
-}
-
-func TestGoogleTestSuite(t *testing.T) {
-	suite.Run(t, new(GoogleTestSuite))
+	assert.EqualError(t, err,
+		"parse ports of firewall: unrecognized port format: 1-80-81")
 }
