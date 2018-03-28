ecfd551a95c58788ba714d978c5489b749f35f9e
Kevin Lin <kevin@kelda.io>
2018-03-27 17:05:04 -0700

    version: Pin to release version 0.13.1

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the Kelda version number.
-const Version = "0.13.0"
+const Version = "0.13.1"
 
 // Image of the kelda container that contains the minion and kubernetes.
 const Image = "keldaio/kelda:" + Version
